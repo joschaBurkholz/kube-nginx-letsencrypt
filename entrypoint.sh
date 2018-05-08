9fa3793aa79f5595fa505219f821ba6aad433b7f
Joscha Burkholz <joscha.burkholz@gmx.de>
2018-05-08 17:45:43 +0200

    remove test certs option

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ NAMESPACE=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
 cd $HOME
 python -m SimpleHTTPServer 80 &
 PID=$!
-certbot certonly --webroot -w $HOME -n --agree-tos --email ${EMAIL} --no-self-upgrade -d ${DOMAINS} --test-cert
+certbot certonly --webroot -w $HOME -n --agree-tos --email ${EMAIL} --no-self-upgrade -d ${DOMAINS}
 kill $PID
 
 CERTPATH=/etc/letsencrypt/live/$(echo $DOMAINS | cut -f1 -d',')
